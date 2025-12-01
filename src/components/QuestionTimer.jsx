2bc6a78b05918fa375d2572fc5bc794fcb151908
dev <dev@test.com>
2025-12-01 21:27:26 +0300

    add a timer progress for each question in new component & add callbacks to reduce the re-renders

---
+++ src/components/QuestionTimer.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+function QuestionTimer({ onFinish, timer }) {
+  const [intervalState, setIntervalState] = useState(timer);
+
+  useEffect(() => {
+    const timerCounter = setTimeout(onFinish, timer);
+
+    // when effect() execute twice || it's component unmount
+    return () => {
+      clearTimeout(timerCounter);
+    };
+  }, [onFinish, timer]); // when done || when new timer (parent re-render)
+
+  // wrap at with "useEffect()" to make it render only 1 time, after 1st render:
+  useEffect(() => {
+    setIntervalState(timer); // reset timer
+    const interval = setInterval(() => {
+      setIntervalState((current) => current - 100);
+    }, 100);
+
+    // since we in 'StrictMode', effect() render 2 times, and this is a case of triggering clean-up() => will remove old effect()
+    return () => {
+      clearInterval(interval); // when got new timer (parent re-render)
+    };
+  }, []);
+
+  return <progress value={intervalState} max={timer} />;
+}
+
+export default QuestionTimer;
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,22 +1,56 @@
-import { useState } from "react";
-import QUESTIONS from "../questions.js";
+import { useState, useEffect, useCallback } from "react";
+import completeImg from "../assets/quiz-complete.png";
 
+import QUESTIONS from "../questions.js";
+import QuestionTimer from "./QuestionTimer.jsx";
+
+const TIMER = 5000;
 function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
-  const userQuestionIndex = userAnswers.length;
 
-  function handleUserAnswer(selectedAnswer) {
+  const userQuestionIndex = userAnswers.length;
+  const isComplete = userQuestionIndex == QUESTIONS.length;
+
+  // callBack:dependency array is empty ([]). This means handleUserAnswer will only be 'created' once
+  const handleUserAnswer = useCallback(function handleUserAnswer(
+    selectedAnswer
+  ) {
     setUserAnswers((current) => {
       return [...current, selectedAnswer];
     });
+  },
+  []);
+
+  const handleNextQuestion = useCallback(() => {
+    handleUserAnswer(null);
+  }, [handleUserAnswer]); // since we make 'handleUserAnswer' with callback & empty [], this also won't be created again
+
+  if (isComplete) {
+    return (
+      <div id="summary">
+        <img src={completeImg} alt="quiz completion" />
+        <h2>Quiz Completed</h2>
+      </div>
+    );
   }
+
+  // grab the current answers index of user:
+  const shuffledAnswers = [...QUESTIONS[userQuestionIndex].answers];
+  // shuffle the answers by: if( - => first,second) & if(+ => second,first):
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
       <div id="question">
+        {/* every new key <=> the component will be re-created again !*/}
+        <QuestionTimer
+          timer={TIMER}
+          onFinish={handleNextQuestion}
+          key={userQuestionIndex}
+        />
         <h2>{QUESTIONS[userQuestionIndex].text}</h2>
         <ul id="answers">{/* output the questions dynamically */}</ul>
-        {QUESTIONS[userQuestionIndex].answers.map((answer) => (
+        {shuffledAnswers.map((answer) => (
           <li className="answer" key={answer}>
             <button onClick={() => handleUserAnswer(answer)}>{answer}</button>
           </li>
@@ -25,5 +59,4 @@ function Quiz() {
     </div>
   );
 }
-
 export default Quiz;
